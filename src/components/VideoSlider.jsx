aaf04634041f9b41cabd9b905006df8214c42c26
ashishbahar <ashishbahar987@gmail.com>
2024-05-22 13:37:47 +0530

    silder and video height change

--- src/components/VideoSlider.jsx
+++ src/components/VideoSlider.jsx
@@ -15,7 +15,7 @@ const VideoSlider = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 3000,
   };
   const slides = [
